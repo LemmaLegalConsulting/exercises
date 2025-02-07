9024247d29946314682c4db005f1997596565b2e
Jim Graszer <jim@lemmalegal.com>
2025-02-06 23:41:12 -0600

    Solution to Gilded Rose Kata

--- gildedrose/gilded_rose.py
+++ gildedrose/gilded_rose.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+MIN = 0
+MAX = 50
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -7,33 +10,38 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+            if item.name == "Aged Brie":
+                if item.quality < MAX:
+                    item.quality += 1
+                if item.sell_in < 1 and item.quality < MAX:
+                    item.quality += 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < MAX:
+                    item.quality += 1
+                    if item.sell_in < 11 and item.quality < MAX:
+                        item.quality += 1
+                    if item.sell_in < 6 and item.quality < MAX:
+                        item.quality += 1
+                    if item.sell_in < 1:
+                        item.quality = MIN
+            elif item.name == "Conjured Mana Cake":
+                if item.sell_in > MIN:
+                    item.quality += -2
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality += -4
+                
+                item.quality = MIN if item.quality < MIN else item.quality
+            else:
+                if item.quality > MIN:
+                    item.quality += -1
+                if item.sell_in < 1 and item.quality > MIN:
+                    item.quality += -1
+                
+                
+
+            item.sell_in += -1
 
 
 class Item:
